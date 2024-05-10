14c5acc3e3fb56a75b3c8d824c28c7556212905a
Ly My Y <lymy7724@gmail.com>
2024-05-10 00:45:30 -0400

    updated everything
    added responsiveness to my pages

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-import data from './products.json' assert{type: 'json'}; // imports json file 
+fetch(`products.json`)
+  .then((response) => response.json()) // parse the response as a JSON
+  .then((data) => {
+    const productContainer = document.getElementById("product-container");
 
-// for each loop - targets each item in the "items" array (data['items'])
-data['items'].forEach((element) => {
-    console.log(element);
-    let name = element["product name"]
-    let price = element["product price"]
-    let details = element["product description"]
-    let picture = element["picture-url"]
+    // for each loop - targets each item in the "items" array (data['items'])
+    data.items.forEach((element) => {
+      console.log(element);
+      let name = element["product name"];
+      let price = element["product price"];
+      let details = element["product description"];
+      let picture = element["picture-url"];
 
-    // create a div element
-    const productCard = document.createElement("div");
+      // create a div element
+      const productCard = document.createElement("div");
 
-    // adds class to div
-    productCard.classList.add("card")
-    const html =
-        `
+      // adds class to div
+      productCard.classList.add("card");
+      const html = `
             <div class="img-hover"><img src="${picture}" class="images"></div>
             <p class="name">${name}</p>
             <p>${details}</p>
             <p class="price">${price}</p>
             <div class="cart2"><button class="cart">ADD TO CART</button></div>
-        `
-    productCard.innerHTML = html
-    document.getElementById("product-container").appendChild(productCard);
-
-});
-
+        `;
+      productCard.innerHTML = html;
+      document.getElementById("product-container").appendChild(productCard);
+    });
+  });
